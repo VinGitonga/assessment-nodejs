dac183d0f456fe4c75b2e3d5a5ff32f2d39d39c7
VinGitonga <gitongavincent64@gmail.com>
2024-11-28 11:09:53 +0300

    added app-datasource config

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,8 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 export const APP_PORT = process.env.APP_PORT;
+export const DB_HOST = process.env.DB_HOST
+export const DB_USERNAME = process.env.DB_USERNAME
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_DATABASE = process.env.DB_DATABASE
+export const JWT_SECRET = process.env.JWT_SECRET
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { APP_PORT } from "./env";
 import { logger } from "./logger/winston";
 import { morganMiddleware } from "./middlewares/morgan.middleware";
+import { AppDataSource } from "./data-source";
 
 const app = express();
 
@@ -16,6 +17,11 @@ app.get("*", (req: express.Request, res: express.Response) => {
   res.status(500).json({ success: false, msg: "Internal Server Error" });
 });
 
-app.listen(APP_PORT, () => {
-  logger.info(`Server started at http://localhost:${APP_PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(APP_PORT, () => {
+      logger.info(`Server started at http://localhost:${APP_PORT}`);
+    });
+    logger.info("App data source has been initialized successfully");
+  })
+  .catch((err) => logger.error("An error was encounted", err));
