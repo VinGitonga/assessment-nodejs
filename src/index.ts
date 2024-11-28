385f36162c52932c2f73d4c103e637742a9ccc43
VinGitonga <gitongavincent64@gmail.com>
2024-11-28 12:11:58 +0300

    added error handler

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import { AppDataSource } from "./data-source";
 import { authRouter } from "./routes/auth.route";
 import { customerRouter } from "./routes/customer.route";
 import { transactionRouter } from "./routes/transaction.route";
+import { errorHandler } from "./middlewares/error.middleware";
 
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler as any);
 app.use(morganMiddleware);
 
 app.get("/", (req: express.Request, res: express.Response) => {
