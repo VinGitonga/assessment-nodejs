8ba4da196587d653a17b5a360a22a01647b882c2
VinGitonga <gitongavincent64@gmail.com>
2024-11-28 10:27:30 +0300

    added app logger

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import * as express from "express";
 import { APP_PORT } from "./env";
+import { logger } from "./logger/winston";
 
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send("Hello World");
 });
 
 app.get("*", (req: express.Request, res: express.Response) => {
-  res.status(500).json({ success: false, msg: "Internale Server Error" });
+  res.status(500).json({ success: false, msg: "Internal Server Error" });
 });
 
 app.listen(APP_PORT, () => {
-  console.log(`Server started at http://localhost:${APP_PORT}`);
+  logger.info(`Server started at http://localhost:${APP_PORT}`);
 });
